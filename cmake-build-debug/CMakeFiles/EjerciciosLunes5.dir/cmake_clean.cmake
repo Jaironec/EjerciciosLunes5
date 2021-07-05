26945bbe76e32369e319b40bb8dc4b0671446a47
Jaironec <16cardenas16@gmail.com>
2021-07-05 17:25:22 -0500

    Ejercicios Lunes 5

---
+++ cmake-build-debug/CMakeFiles/EjerciciosLunes5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EjerciciosLunes5.dir/main.cpp.obj"
+  "EjerciciosLunes5.exe"
+  "EjerciciosLunes5.exe.manifest"
+  "EjerciciosLunes5.pdb"
+  "libEjerciciosLunes5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EjerciciosLunes5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
